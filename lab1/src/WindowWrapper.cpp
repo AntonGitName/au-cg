31a8d55dc6f30868cfcbec96264d912e6206b5c1
Anton <mordbergak@gmail.com>
2017-01-07 08:00:37 +0300

    osx update

--- lab1/src/WindowWrapper.cpp
+++ lab1/src/WindowWrapper.cpp
@@ -14,6 +14,7 @@ WindowWrapper::WindowWrapper(int w, int h, const char* title) {
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     if (!(window = glfwCreateWindow(w, h, title, NULL, NULL))) {
