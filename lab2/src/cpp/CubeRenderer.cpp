8d3979cddace582b24819397e307070bfffa01d7
Anton Mordberg <mordbergak@gmail.com>
2017-01-09 15:32:56 +0300

    add texture mipmap

--- lab2/src/cpp/CubeRenderer.cpp
+++ lab2/src/cpp/CubeRenderer.cpp
@@ -31,6 +31,9 @@ void CubeRenderer::load_cubemap() {
         glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
         SOIL_free_image_data(image);
     }
+
+    glGenerateMipmap(cubemap_id);
+
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
