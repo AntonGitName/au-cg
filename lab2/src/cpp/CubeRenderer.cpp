8f33c87e411a497a28ac4094a6dd724967ca8d8f
Anton Mordberg <mordbergak@gmail.com>
2017-01-13 21:45:05 +0300

    lab 2 : MIP filtration fix

--- lab2/src/cpp/CubeRenderer.cpp
+++ lab2/src/cpp/CubeRenderer.cpp
@@ -32,10 +32,11 @@ void CubeRenderer::load_cubemap() {
         SOIL_free_image_data(image);
     }
 
-    glGenerateMipmap(cubemap_id);
+    glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
 
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
